b39afa41e955da72badd43f9ccfab887dbf6bf2e
PRGAnna <mdina@gmx.net>
2017-06-22 12:17:39 +0200

    Unity Test Projekt

---
+++ ProjektBTest/Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+
+
+	public Texture aTexture;
+
+	private bool enteredTrigger = false;
+
+	void OnGui() {
+		if (!aTexture) {
+			Debug.LogError("Assign a Texture in the inspector.");
+			return;
+		}
+		if(enteredTrigger == true && Input.GetKey(KeyCode.E)){
+		GUI.DrawTexture(new Rect(10, 10, 60, 60), aTexture, ScaleMode.ScaleToFit, true, 10.0F);
+		}
+	}
+
+	void  OnTriggerStay (Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			enteredTrigger = true;
+			Debug.Log ("hit");
+		}
+	}
+
+	void  OnTriggerExit (Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			enteredTrigger = false;
+		}
+	}
+}
---
+++ ProjektBTest/Assets/Scripts/GuiShow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiShow : MonoBehaviour {
+
+	bool guiShow = false;
+	[SerializeField] private Texture text;
+
+	void start() {
+		guiShow = true;
+	}
+
+	void update () {
+		if(Input.GetKeyDown(KeyCode.E) && guiShow == true){
+			//if(guiShow == true){
+				GUI.DrawTexture(new Rect(10, 10, 60, 60), text, ScaleMode.ScaleToFit, true, 10.0F);
+			}
+		}
+
+	}
