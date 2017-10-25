60fc8b05e32b465ef7645aadecd8805708fe1036
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2017-10-25 17:02:05 +0200

    Skripte für Menü und Bildmanagement hinzugefügt

---
+++ Unity Projekt/Scripts/MangagePictures.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MangagePictures : MonoBehaviour {
+	public Image image;
+	public Sprite newCoverImage;
+	
+	public Button umdrehen;
+	public Button next;
+	public Button zurück;
+	private int x;
+	
+	public Sprite[] imageList;
+	// Use this for initialization
+	void Start () {
+		x = -1;
+		next.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void nextPic()
+	{
+		x++;
+		zurück.gameObject.SetActive(true);
+		if(x==imageList.Length-1){
+			next.gameObject.SetActive(false);
+		}
+		image.sprite = imageList[x];
+		
+	}
+	
+	public void beforePic(){
+		x--;
+		if(x==-1){
+			image.sprite = newCoverImage;
+			zurück.gameObject.SetActive(false);
+			next.gameObject.SetActive(true);
+		}
+		image.sprite = imageList[x];
+	}
+}
---
+++ Unity Projekt/Scripts/OnClick_ObjectMenu.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class OnClick_ObjectMenu : MonoBehaviour {
+	
+	public Transform canvas;
+	public Transform Player;
+	
+	public Image image;
+	public Sprite newCoverImage;
+	
+	//public Button umdrehen;
+	//public Button next;
+	//public Button zurück;
+	//private int x;
+	
+	
+	//public Sprite[] imageList;
+	
+	
+	void Start () 
+	{
+		//x = 0;
+		Cursor.visible = false;
+		Canvas canvas = GetComponent<Canvas>();
+		//image = canvas.GetComponent<Sprite>();	
+		/*if (imageList[0] != null){
+			next.gameObject.SetActive(true);
+			}*/
+	}
+	
+	void OnMouseDown()
+	{
+       Pause();
+	   image.sprite = newCoverImage;
+	}   
+
+	
+	
+	public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+			Cursor.lockState = CursorLockMode.None; 
+            Cursor.visible = true;
+			canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            Player.GetComponent<FirstPersonController>().enabled = false;
+			
+        }
+    }
+	
+	public void PauseEnde()
+    {
+        if (canvas.gameObject.activeInHierarchy == true)
+        {
+			Cursor.visible = false;
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            Player.GetComponent<FirstPersonController>().enabled = true;
+        }
+    }
+	
+	void Update () 
+	{
+	}
+	
+	/*public void nextPic()
+	{
+		zurück.gameObject.SetActive(true);
+		//image.sprite = newImage1;
+		
+		for(int i = x; i < imageList.Length ; i++){
+			x=i;
+			image.sprite = imageList[x];
+		}
+		
+	}
+	
+	public void beforePic(){
+		for(int i = x; i < imageList.Length ; i--){
+			x=i;
+			image.sprite = imageList[x];
+		}
+		
+	}*/
+	
+	
+}
