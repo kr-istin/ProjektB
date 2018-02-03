1dc4b3e59be1cf20277aaf6a639a29462d28bc50
PRGAnna <mdina@gmx.net>
2018-02-03 17:44:39 +0100

    Neue Szene "Reminiscence"

---
+++ Unity Projekt/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour {
+
+	/*public void  ModeSelect(){
+		StartCoroutine("Wait");
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(6);
+		Application.LoadLevel("House");
+	}*/
+	void Start(){
+		Invoke ("Load", 5);
+	}
+
+
+	void Load(){
+		SceneManager.LoadScene ("House");
+	}
+}
